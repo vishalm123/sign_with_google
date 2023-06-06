a015a256972ef771739e906eaa226b79074a8183
vishalm123 <83279990+vishalm123@users.noreply.github.com>
2023-06-07 00:28:57 +0530

    use console.log in index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    $("#name").text(profile.getName());
-    $("#email").text(profile.getEmail());
-    $("#img").attr('src', profile.getImageUrl());
-    $(".data").css("display","block");
-    $(".g-signin2").css("display","none");
-  }
-
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
